ee11060b82782dfdf05769c0457e5d02b5ab9213
valentin <valou.lacour59@gmail.com>
2020-11-05 00:57:04 +0100

    dev

--- base/function_maj.py
+++ base/function_maj.py
@@ -7,6 +7,9 @@ def maj_git():
     subprocess.run("git push", shell=True)
 
 def maj_pqt():
+    subprocess.run("sudo apt-get update", shell=True)
+    subprocess.run("sudo apt-get install python3.8", shell=True)
+    subprocess.run("sudo apt install python3-pip", shell=True)
     subprocess.run("sudo apt-get upgrade", shell=True)
     subprocess.run("pip install -r requirements.txt", shell=True)
     subprocess.run("pip3 install -r requirements.txt", shell=True)
