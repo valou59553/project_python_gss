888151018ba357c3af710f426da2ab2156d9d3be
valentin <valou.lacour59@gmail.com>
2020-11-04 21:02:41 +0100

    dev

--- base/convertisseur.py
+++ base/convertisseur.py
@@ -8,7 +8,7 @@ env = {'source': sys.argv[1], 'destination': sys.argv[2]}
 def main():    
     # open website on github 
     if not os.environ.get("WERKZEUG_RUN_MAIN"):
-        url = 'https://valou59553.github.io/project_python_gss/%s/'%(env['destination'])
+        url = 'https://valou59553.github.io/project_python_gss/%s/index.html'%(env['destination'])
         webbrowser.open_new(url)
 
 def change_name_files(path, extension='.md'):
