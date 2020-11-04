d351edfdb3a62aabfe878d2af19491cfbd27b90c
valentin <valou.lacour59@gmail.com>
2020-11-04 19:31:59 +0100

    dev

--- base/function_git.py
+++ base/function_git.py
@@ -1,6 +1,6 @@
 import subprocess
 
 def push_on_git():
-    subprocess.run("git add .", shell=True)
+    subprocess.run("git add ..", shell=True)
     subprocess.run("git commit -a -m 'dev'", shell=True)
     subprocess.run("git push", shell=True)
